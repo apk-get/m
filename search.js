92668d7f0c21b34da9d9df439bd74a5cf13713e2
apk-get <89330779+apk-get@users.noreply.github.com>
2021-12-14 16:23:47 +0800

    add search function

---
+++ search.js
@@ -0,0 +1,244 @@
+function openPage() { var x = document.getElementById("search").value; 
+	if (x === "facebook lite") 
+	{ window.open("facebooklite.html"); } 
+	
+	if (x === "facebook") 
+	{ window.open("facebook.html"); } 
+	
+	if (x === "messenger lite") 
+	{ window.open("messengerlite.html"); } 
+	
+	if (x === "messenger") 
+	{ window.open("messenger.html"); } 
+	
+	if (x === "twitter") 
+	{ window.open("twitter.html"); } 
+	
+	if (x === "google meet") 
+	{ window.open("gmeet.html"); } 
+	
+	if (x === "google classroom") 
+	{ window.open("gclassroom.html"); } 
+	
+	if (x === "twitter lite") 
+	{ window.open("resulttwitterlite.html"); } 
+	
+	if (x === "zombie catchers") 
+	{ window.open("resultzombiecatchers.html"); } 
+	
+	if (x === "zombie tsunami") 
+	{ window.open("resultzombietsunami.html"); } 
+	
+	if (x === "templerun") 
+	{ window.open("resulttemplerun.html"); } 
+	
+	if (x === "plants vs zombies") 
+	{ window.open("pvz.html"); } 
+	
+	if (x === "cooking fever") 
+	{ window.open("cookingfever.html"); } 
+	
+	if (x === "pooking billiards") 
+	{ window.open("pookingbilliards.html"); } 
+	
+	if (x === "pixellab") 
+	{ window.open("pixellab.html"); } 
+	
+	if (x === "twisty timer") 
+	{ window.open("twistytimer.html"); } 
+	
+	if (x === "wps office") 
+	{ window.open("wpsoffice.html"); } 
+	
+	if (x === "shareit lite") 
+	{ window.open("shareitlite.html"); } 
+	
+	if (x === "turbovpn lite") 
+	{ window.open("turbovpnlite.html"); } 
+	
+	if (x === "sortpuz") 
+	{ window.open("sortpuz.html"); } 
+	
+	if (x === "mobile legends") 
+	{ window.open("mobilelegends.html"); } 
+	
+	if (x === "pba slam") 
+	{ window.open("phslam.html"); } 
+	
+	if (x === "dumb ways to die") 
+	{ window.open("dumbwaystodie.html"); } 
+	
+	if (x === "specimen zero") 
+	{ window.open("specimen.html"); } 
+	
+	if (x === "remini") 
+	{ window.open("remini.html"); } 
+	
+	if (x === "clash of clans") 
+	{ window.open("coc.html"); } 
+	
+	if (x === "pinterest") 
+	{ window.open("pinterest.html"); } 
+	
+	if (x === "duckduckgo") 
+	{ window.open("duckduckgo.html"); } 
+	
+	if (x === "zarchiver") 
+	{ window.open("zarchiver.html"); } 
+	
+	if (x === "among us") 
+	{ window.open("amongus.html"); } 
+	
+	if (x === "netflix") 
+	{ window.open("netflix.html"); } 
+	
+	if (x === "spotify") 
+	{ window.open("spotify.html"); } 
+	
+	if (x === "wattpad") 
+	{ window.open("wattpad.html"); } 
+	
+	if (x === "ppsspp") 
+	{ window.open("ppsspp.html"); } 
+	
+	if (x === "slither") 
+	{ window.open("slither.html"); } 
+	
+	if (x === "wordscapes") 
+	{ window.open("wordscapes.html"); } 
+	
+	if (x === "gcash") 
+	{ window.open("gcash.html"); } 
+	
+	if (x === "webtoon") 
+	{ window.open("webtoon.html"); } 
+	
+	if (x === "shopee") 
+	{ window.open("shopee.html"); } 
+	
+	if (x === "pubg lite") 
+	{ window.open("pubglite.html"); } 
+	
+	if (x === "genshin impact") 
+	{ window.open("genshin.html"); } 
+	
+	if (x === "clash royale") 
+	{ window.open("clashroyale.html"); } 
+	
+	if (x === "honkai impact 3") 
+	{ window.open("honkai.html"); } 
+	
+	if (x === "zombie castaways") 
+	{ window.open("zombiecastaways.html"); } 
+	
+	if (x === "call of duty") 
+	{ window.open("cod.html"); } 
+	
+	if (x === "freefire max") 
+	{ window.open("freefire.html"); } 
+	
+	if (x === "xapk installer") 
+	{ window.open("xapkinstaller.html"); } 
+	
+	if (x === "youtube") 
+	{ window.open("youtube.html"); } 
+	
+	if (x === "wildrift") 
+	{ window.open("lolwildrift.html"); } 
+	
+	if (x === "cafe panic") 
+	{ window.open("cafepanic.html"); } 
+	
+	if (x === "cooking madness") 
+	{ window.open("cooking madness.html"); } 
+	
+	if (x === "spongebob krusty cook off") 
+	{ window.open("spongebobkrustycookoff.html"); } 
+	
+	if (x === "thetan arena") 
+	{ window.open("thetan arena.html"); } 
+	
+	if (x === "pokemon unite") 
+	{ window.open("pokemonunite.html"); } 
+	
+	if (x === "good pizza great pizza") 
+	{ window.open("goodpizzagreatpizza.html"); } 
+	
+	if (x === "ai grammar checker") 
+	{ window.open("aigrammarchecker.html"); } 
+	
+	if (x === "merriam webster") 
+	{ window.open("merriamwebster.html"); } 
+	
+	if (x === "photomath") 
+	{ window.open("photomath.html"); } 
+	
+	if (x === "gauthmath") 
+	{ window.open("gauthmath.html"); } 
+	
+	if (x === "brainly") 
+	{ window.open("brainly.html"); } 
+	
+	if (x === "school planner") 
+	{ window.open("schoolplanner.html"); } 
+	
+	if (x === "airplane chefs") 
+	{ window.open("airplanechefs.html"); } 
+	
+	if (x === "tiktok lite") 
+	{ window.open("tiktoklite.html"); } 
+	
+	if (x === "tiktok") 
+	{ window.open("tiktok.html"); } 
+	
+	if (x === "heroes strike offline") 
+	{ window.open("heroesstrikeoffline.html"); } 
+	
+	if (x === "marvel super war") 
+	{ window.open("marvelsuperwar.html"); } 
+	
+	if (x === "instagram lite") 
+	{ window.open("instagramlite.html"); } 
+	
+	if (x === "paymaya") 
+	{ window.open("paymaya.html"); } 
+	
+	if (x === "spotify lite") 
+	{ window.open("spotifylite.html"); } 
+	
+	if (x === "legend of ace") 
+	{ window.open("legendoface.html"); } 
+	
+	if (x === "foodpanda") 
+	{ window.open("foodpanda.html"); } 
+	
+	if (x === "mangatoon") 
+	{ window.open("mangatoon.html"); } 
+	
+	if (x === "mangatoon lite") 
+	{ window.open("mangatoonlite.html"); } 
+	
+	if (x === "unionbank online") 
+	{ window.open("unionbank online.html"); } 
+	
+	if (x === "heroes evolved") 
+	{ window.open("heroesevolved.html"); } 
+	
+	if (x === "lazada") 
+	{ window.open("lazada.html"); } 
+	
+	if (x === "paypal") 
+	{ window.open("paypal.html"); } 
+	
+	if (x === "NAME") 
+	{ window.open("FILE.html"); } 
+	
+	if (x === "NAME") 
+	{ window.open("FILE.html"); } 
+	
+	if (x === "NAME") 
+	{ window.open("FILE.html"); } 
+	
+	if (x === "NAME") 
+	{ window.open("FILE.html"); } 
+	} 
